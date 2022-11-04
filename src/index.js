37c4c2ffb8d954e8af5ed87f93a349897a001092
Maxim <m.aleynikov@vpene.ru>
2022-11-04 21:13:57 +0300

    Edits

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./css/style.scss";
 import { AppComponent } from "./components/app.component";
+import { StatusBar } from "@capacitor/status-bar";
+
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  StatusBar.hide().then(() => {});
+}
 
 new AppComponent("app");
