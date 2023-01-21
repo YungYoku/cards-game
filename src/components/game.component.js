c23c3c9669c0258775e1f427cd58d020c6c6ec80
Maxim <yungyoku@yandex.ru>
2023-01-21 20:26:52 +0300

    Edits

--- src/components/game.component.js
+++ src/components/game.component.js
@@ -1,6 +1,6 @@
-import { Component } from "../core/component";
-import { SettingsStore } from "../stores/settings.store";
-import { CardComponent } from "./card.component";
+import {Component} from "../core/component";
+import {SettingsStore} from "../stores/settings.store";
+import {CardComponent} from "./card.component";
 import card1 from "../assets/images/game/cards/1.png";
 import card2 from "../assets/images/game/cards/2.png";
 import card3 from "../assets/images/game/cards/3.png";
@@ -19,255 +19,256 @@ import card15 from "../assets/images/game/cards/15.png";
 import card16 from "../assets/images/game/cards/16.png";
 import card17 from "../assets/images/game/cards/17.png";
 import card18 from "../assets/images/game/cards/18.png";
-import { TimerComponent } from "./timer.component";
-import { PausePopup } from "./pause.popup";
-import { WinPopup } from "./win.popup";
-import { LosePopup } from "./lose.popup";
+import {TimerComponent} from "./timer.component";
+import {PausePopup} from "./pause.popup";
+import {WinPopup} from "./win.popup";
+import {LosePopup} from "./lose.popup";
 
 export class GameComponent extends Component {
-  constructor(className, options) {
-    super(className);
+    constructor(className, options) {
+        super(className);
 
-    this.sound = options.sound;
+        this.sound = options.sound;
 
-    this.cards = [];
-    this.cardImages = [
-      card1,
-      card2,
-      card3,
-      card4,
-      card5,
-      card6,
-      card7,
-      card8,
-      card9,
-      card10,
-      card11,
-      card12,
-      card13,
-      card14,
-      card15,
-      card16,
-      card17,
-      card18,
-    ];
-    this.destroyedCards = 0;
-    this.firstCardId = this.secondCardId = 0;
-    this.isAnimationGoing = false;
-    this.animationTime = 500;
+        this.cards = [];
+        this.cardImages = [
+            card1,
+            card2,
+            card3,
+            card4,
+            card5,
+            card6,
+            card7,
+            card8,
+            card9,
+            card10,
+            card11,
+            card12,
+            card13,
+            card14,
+            card15,
+            card16,
+            card17,
+            card18,
+        ];
+        this.destroyedCards = 0;
+        this.firstCardId = this.secondCardId = 0;
+        this.isAnimationGoing = false;
+        this.animationTime = 500;
 
-    this.pause = new PausePopup("game__pause", { music: options.music });
-    this.win = new WinPopup("game__win");
-    this.lose = new LosePopup("game__lose");
-    this.timer = new TimerComponent("game__info__timer__time", this.lose);
+        this.pause = new PausePopup("game__pause", {music: options.music});
+        this.win = new WinPopup("game__win");
+        this.lose = new LosePopup("game__lose");
+        this.timer = new TimerComponent("game__info__timer__time", this.lose);
 
-    this.pause.close = () => {
-      this.pause.hide();
-    };
-    this.pause.restart = () => {
-      this.startGame();
-      this.unpauseGame();
-    };
-    this.pause.leave = () => {
-      this.beforeHideGame();
-      this.pause.hide();
-      setTimeout(() => {
-        this.timer.reset();
-        this.hide();
-      }, 1000);
-    };
+        this.pause.close = () => {
+            this.pause.hide();
+        };
+        this.pause.restart = () => {
+            this.startGame();
+            this.unpauseGame();
+        };
+        this.pause.leave = () => {
+            this.beforeHideGame();
+            this.pause.hide();
+            setTimeout(() => {
+                this.timer.reset();
+                this.hide();
+            }, 1000);
+        };
 
-    this.win.onHide = () => {
-      this.$el.querySelector(".game__board").innerHTML = "";
-      this.cards = [];
-    };
-    this.win.leave = () => {
-      this.win.hide();
-      this.leave();
-    };
-    this.win.restart = () => {
-      this.win.hide();
-      this.restart();
-    };
+        this.win.onHide = () => {
+            this.$el.querySelector(".game__board").innerHTML = "";
+            this.cards = [];
+        };
+        this.win.leave = () => {
+            this.win.hide();
+            this.leave();
+        };
+        this.win.restart = () => {
+            this.win.hide();
+            this.restart();
+        };
 
-    this.lose.leave = () => {
-      this.lose.hide();
-      this.leave();
-    };
-    this.lose.restart = () => {
-      this.lose.hide();
-      this.restart();
-    };
-  }
-
-  onInit() {
-    this.$el.querySelector(".game__board").innerHTML = "";
-    this.$el
-      .querySelector(".game__board")
-      .addEventListener("click", cardClickHandler.bind(this));
-    this.$el
-      .querySelector(".game__info")
-      .addEventListener("click", topBarClickHandler.bind(this));
-  }
-
-  onShow() {
-    this.startGame();
-  }
-
-  beforeHideGame() {}
-
-  startGame() {
-    this.cards = [];
-    this.destroyedCards = 0;
-    this.firstCardId = this.secondCardId = 0;
-    this.isAnimationGoing = false;
-    this.animationTime = 500;
-    this.$el.querySelector(".game__board").innerHTML = "";
-    this.shuffleCards();
-    this.renderCards();
-    this.timer.show();
-  }
-
-  startAnimation() {
-    this.isAnimationGoing = true;
-  }
-
-  endAnimation() {
-    setTimeout(() => {
-      this.isAnimationGoing = false;
-    }, this.animationTime);
-  }
-
-  renderCards() {
-    this.$el.querySelector(".game__board").innerHTML = "";
-
-    let gameSize = "game-3x3";
-    switch (Number(SettingsStore.getCardsAmount())) {
-      case 8:
-        gameSize = "game-3x3";
-        break;
-      case 16:
-        gameSize = "game-4x4";
-        break;
-      case 24:
-        gameSize = "game-5x5";
-        break;
-      case 36:
-        gameSize = "game-6x6";
-        break;
+        this.lose.leave = () => {
+            this.lose.hide();
+            this.leave();
+        };
+        this.lose.restart = () => {
+            this.lose.hide();
+            this.restart();
+        };
     }
-    this.$el
-      .querySelector(".game__board")
-      .classList.remove("game-3x3", "game-4x4", "game-5x5", "game-6x6");
-    this.$el.querySelector(".game__board").classList.add(gameSize);
 
-    this.cards.forEach((card) => {
-      this.$el
-        .querySelector(".game__board")
-        .insertAdjacentHTML("beforeend", card.renderCard());
-    });
-  }
-
-  shuffleCards() {
-    const ids = [];
-    for (let i = 1; i <= SettingsStore.getCardsAmount(); i++) {
-      ids.push(i);
+    onInit() {
+        this.$el.querySelector(".game__board").innerHTML = "";
+        this.$el
+            .querySelector(".game__board")
+            .addEventListener("click", cardClickHandler.bind(this));
+        this.$el
+            .querySelector(".game__info")
+            .addEventListener("click", topBarClickHandler.bind(this));
     }
-    for (let i = 1; i <= SettingsStore.getCardsAmount(); i++) {
-      let randId = Math.floor(Math.random() * ids.length);
-      const newCard = new CardComponent({
-        number: (i % (SettingsStore.getCardsAmount() / 2)) + 1,
-        image: this.cardImages[i % (SettingsStore.getCardsAmount() / 2)],
-        id: ids[randId],
-      });
-      ids.splice(randId, 1);
-      this.cards.push(newCard);
+
+    onShow() {
+        this.startGame();
     }
-  }
 
-  pauseGame() {
-    this.timer.pauseTimer();
-    this.pause.show();
-  }
+    beforeHideGame() {
+    }
 
-  unpauseGame() {
-    this.timer.unpauseTimer();
-    this.pause.hide();
-  }
+    startGame() {
+        this.cards = [];
+        this.destroyedCards = 0;
+        this.firstCardId = this.secondCardId = 0;
+        this.isAnimationGoing = false;
+        this.animationTime = 500;
+        this.$el.querySelector(".game__board").innerHTML = "";
+        this.shuffleCards();
+        this.renderCards();
+        this.timer.show();
+    }
 
-  restart() {
-    this.timer.startTimer();
-    this.timer.unpauseTimer();
-    this.startGame();
-  }
+    startAnimation() {
+        this.isAnimationGoing = true;
+    }
 
-  leave() {
-    this.lose.hide();
-    this.pause.leave();
-  }
+    endAnimation() {
+        setTimeout(() => {
+            this.isAnimationGoing = false;
+        }, this.animationTime);
+    }
 
-  endGame() {
-    this.timer.pauseTimer();
-    this.win.show();
-    this.win.win(this.timer.getTime());
-  }
+    renderCards() {
+        this.$el.querySelector(".game__board").innerHTML = "";
+
+        let gameSize = "game-3x3";
+        switch (Number(SettingsStore.getCardsAmount())) {
+            case 8:
+                gameSize = "game-3x3";
+                break;
+            case 16:
+                gameSize = "game-4x4";
+                break;
+            case 24:
+                gameSize = "game-5x5";
+                break;
+            case 36:
+                gameSize = "game-6x6";
+                break;
+        }
+        this.$el
+            .querySelector(".game__board")
+            .classList.remove("game-3x3", "game-4x4", "game-5x5", "game-6x6");
+        this.$el.querySelector(".game__board").classList.add(gameSize);
+
+        this.cards.forEach((card) => {
+            this.$el
+                .querySelector(".game__board")
+                .insertAdjacentHTML("beforeend", card.renderCard());
+        });
+    }
+
+    shuffleCards() {
+        const ids = [];
+        for (let i = 1; i <= SettingsStore.getCardsAmount(); i++) {
+            ids.push(i);
+        }
+        for (let i = 1; i <= SettingsStore.getCardsAmount(); i++) {
+            let randId = Math.floor(Math.random() * ids.length);
+            const newCard = new CardComponent({
+                number: (i % (SettingsStore.getCardsAmount() / 2)) + 1,
+                image: this.cardImages[i % (SettingsStore.getCardsAmount() / 2)],
+                id: ids[randId],
+            });
+            ids.splice(randId, 1);
+            this.cards.push(newCard);
+        }
+    }
+
+    pauseGame() {
+        this.timer.pauseTimer();
+        this.pause.show();
+    }
+
+    unpauseGame() {
+        this.timer.unpauseTimer();
+        this.pause.hide();
+    }
+
+    restart() {
+        this.timer.startTimer();
+        this.timer.unpauseTimer();
+        this.startGame();
+    }
+
+    leave() {
+        this.lose.hide();
+        this.pause.leave();
+    }
+
+    endGame() {
+        this.timer.pauseTimer();
+        this.win.show();
+        this.win.win(this.timer.getTime());
+    }
 }
 
 async function cardClickHandler(e) {
-  if (
-    e.target.dataset.id &&
-    !this.isAnimationGoing &&
-    e.target.dataset.usefull === "true"
-  ) {
-    this.sound.play();
+    if (
+        e.target.dataset.id &&
+        !this.isAnimationGoing &&
+        e.target.dataset.usefull === "true"
+    ) {
+        this.sound.play();
 
-    if (this.firstCardId && this.firstCardId === Number(e.target.dataset.id)) {
-      this.cards.find((el) => el.id === this.firstCardId).rotateCard();
-      this.firstCardId = 0;
-      return;
-    } else if (!this.firstCardId) {
-      this.firstCardId = Number(e.target.dataset.id);
-    } else {
-      this.secondCardId = Number(e.target.dataset.id);
-      this.startAnimation();
-      if (
-        this.cards.find((el) => el.id === this.firstCardId).number ===
-        this.cards.find((el) => el.id === this.secondCardId).number
-      ) {
-        this.cards.find((el) => el.id === this.secondCardId).rotateCard();
-        await new Promise((resolve) => {
-          setTimeout(() => {
-            this.cards.find((el) => el.id === this.firstCardId).destroyCard();
-            this.cards.find((el) => el.id === this.secondCardId).destroyCard();
-            this.destroyedCards += 2;
-            this.endAnimation();
-            setTimeout(() => {
-              this.firstCardId = this.secondCardId = 0;
-              if (this.destroyedCards === this.cards.length) this.endGame();
-              resolve();
-            }, this.animationTime);
-          }, this.animationTime);
-        });
-      } else {
-        this.cards.find((el) => el.id === this.secondCardId).rotateCard();
-        await new Promise((resolve) => {
-          setTimeout(() => {
-            this.endAnimation();
-            setTimeout(() => {
-              this.cards.find((el) => el.id === this.firstCardId).rotateCard();
-              this.cards.find((el) => el.id === this.secondCardId).rotateCard();
-              this.firstCardId = this.secondCardId = 0;
-              resolve();
-            }, this.animationTime);
-          }, this.animationTime);
-        });
-      }
-      return;
+        if (this.firstCardId && this.firstCardId === Number(e.target.dataset.id)) {
+            this.cards.find((el) => el.id === this.firstCardId).rotateCard();
+            this.firstCardId = 0;
+            return;
+        } else if (!this.firstCardId) {
+            this.firstCardId = Number(e.target.dataset.id);
+        } else {
+            this.secondCardId = Number(e.target.dataset.id);
+            this.startAnimation();
+            if (
+                this.cards.find((el) => el.id === this.firstCardId).number ===
+                this.cards.find((el) => el.id === this.secondCardId).number
+            ) {
+                this.cards.find((el) => el.id === this.secondCardId).rotateCard();
+                await new Promise((resolve) => {
+                    setTimeout(() => {
+                        this.cards.find((el) => el.id === this.firstCardId).destroyCard();
+                        this.cards.find((el) => el.id === this.secondCardId).destroyCard();
+                        this.destroyedCards += 2;
+                        this.endAnimation();
+                        setTimeout(() => {
+                            this.firstCardId = this.secondCardId = 0;
+                            if (this.destroyedCards === this.cards.length) this.endGame();
+                            resolve();
+                        }, this.animationTime);
+                    }, this.animationTime);
+                });
+            } else {
+                this.cards.find((el) => el.id === this.secondCardId).rotateCard();
+                await new Promise((resolve) => {
+                    setTimeout(() => {
+                        this.endAnimation();
+                        setTimeout(() => {
+                            this.cards.find((el) => el.id === this.firstCardId).rotateCard();
+                            this.cards.find((el) => el.id === this.secondCardId).rotateCard();
+                            this.firstCardId = this.secondCardId = 0;
+                            resolve();
+                        }, this.animationTime);
+                    }, this.animationTime);
+                });
+            }
+            return;
+        }
+        this.cards.find((el) => el.id === this.firstCardId).rotateCard();
     }
-    this.cards.find((el) => el.id === this.firstCardId).rotateCard();
-  }
 }
 
 function topBarClickHandler(e) {
-  if (e.target.dataset.action === "pause") this.pauseGame();
+    if (e.target.dataset.action === "pause") this.pauseGame();
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import "./css/style.scss";
 import { AppComponent } from "./components/app.component";
-import { StatusBar } from "@capacitor/status-bar";
-import { SplashScreen } from "@capacitor/splash-screen";
 
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  StatusBar.hide().then(() => {});
-  SplashScreen.hide().then(() => {});
-}
+window.screen.orientation.lock("portrait");
 
 new AppComponent("app");
