2d9a43d90cdb958c3094e48ebb829c67f14be755
Maxim <yungyoku@yandex.ru>
2023-01-21 20:35:42 +0300

    Edits

--- src/components/pause.popup.js
+++ src/components/pause.popup.js
@@ -1,43 +1,54 @@
-import { Popup } from "../core/popup";
+import {Popup} from "../core/popup";
 
 export class PausePopup extends Popup {
-  constructor(className, options) {
-    super(className);
+    constructor(className, options) {
+        super(className);
 
-    this.music = options.music;
-  }
+        this.video = this.$el.querySelector(".pause_video");
+        this.music = options.music;
+    }
 
-  onInit() {
-    this.$el.addEventListener("click", clickHandler.bind(this));
-  }
+    onInit() {
+        this.$el.addEventListener("click", clickHandler.bind(this));
+    }
 
-  onShow() {
-    this.music.control.muted = true;
-  }
+    onShow() {
+        this.music.control.muted = true;
+    }
 
-  onHide() {
-    this.music.control.muted = false;
-  }
+    onHide() {
+        this.music.control.muted = false;
+    }
 
-  close() {}
+    close() {
+    }
 
-  restart() {}
+    restart() {
+    }
 
-  leave() {}
+    leave() {
+    }
+
+    closeVideo() {
+        this.video.classList.add("hide");
+    }
 }
 
 function clickHandler(e) {
-  if (e.target.dataset.action) {
-    switch (e.target.dataset.action) {
-      case "restart":
-        this.restart();
-        break;
-      case "leave":
-        this.leave();
-        break;
-      case "close":
-        this.close();
-        break;
+    if (e.target.dataset.action) {
+        switch (e.target.dataset.action) {
+            case "restart":
+                this.restart();
+                break;
+            case "leave":
+                this.leave();
+                break;
+            case "close":
+                this.close();
+                break;
+            case "closeVideo":
+                this.closeVideo();
+                break;
+        }
     }
-  }
 }
